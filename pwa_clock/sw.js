eabd536ca9d1fbb47f5799e73f4916e3a85e8aa8
Caio Fábio Juvino de Queiroz <caio.queiroz@ccc.ufcg.edu.br>
2023-09-05 17:06:54 -0300

    Fix paths

--- pwa_clock/sw.js
+++ pwa_clock/sw.js
@@ -1,16 +1,23 @@
-self.addEventListener("install", e => {
-    console.log("Install!");
-    e.waitUntil(
-        caches.open("static").then(cache => {
-            return cache.addAll(["./", "./src/master.css","./images/logo192.png"]);
-        })
-    );
+self.addEventListener("install", (e) => {
+  console.log("Install!");
+  e.waitUntil(
+    caches.open("static").then((cache) => {
+      return cache.addAll([
+        "./",
+        "./index.html",
+        "./src/master.css",
+        "./src/index.js",
+        "./images/logo192.png",
+        "./images/logo512.png",
+      ]);
+    })
+  );
 });
 
-self.addEventListener("fetch", e => {
-    e.respondWith(
-        caches.match(e.request).then(response => {
-            return response || fetch(e.request);
-        })
-    );
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
 });
